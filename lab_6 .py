7b9051a0593fb1fd1238bdcb121383fc7635d340
AndreySkopich <134918473+AndreySkopich@users.noreply.github.com>
2023-05-31 23:27:50 +0700

    надеюсь, что последняя

--- lab_6 .py
+++ lab_6 .py
@@ -34,7 +34,7 @@ slider.grid(column=0, row=1)
 cur_speed = 1
 
 class Planet:
-    def __init__(self, name, size, color, radius, speed, angle, temp, T, E):
+    def __init__(self, name, size, color, radius, speed, angle, temp, T, E, atmo, diam, grav, mass):
         self.name = name  # имя планеты
         self.size = size  # размер планеты
         self.color = color  # цвет планеты
@@ -46,7 +46,10 @@ class Planet:
         self.temp = temp
         self.T = T
         self.E = E
-        
+        self.atmo = atmo
+        self.diam = diam
+        self.grav = grav
+        self.mass = mass
         # создаем планету на canvas
         self.body = canvas.create_oval(0, 0, size, size, fill=color, outline=color, tags = "planet")
 
@@ -80,13 +83,14 @@ class Planet:
         title = Label(frame, font = ('Times New Roman',20,'bold'), bg = 'black', fg='white', text=self.name)
         title.grid(row=1,column=3)
         
-        empty_label = Label(frame, text= "____", bg="black", fg="black")
-        empty_label.grid(row=2,column=3)
+        empty_label1 = Label(frame, text= "____", bg="black", fg="black")
+        empty_label1.grid(row=3,column=3)
         
         temperature = Label(frame, font = ('Times New Roman',16,'bold'), bg = 'black', fg='white', text = ('Температура: '+str(self.temp)+' градусов'))
         temperature.grid(row=3,column=3)
         
-        empty_label.grid(row=4,column=3)
+        empty_label2 = Label(frame, text= "____", bg="black", fg="black")
+        empty_label2.grid(row=4,column=3)
         
         orbital = Label(frame, font =('Times New Roman',20,'bold'), bg = 'black', fg='white', text="Орбитальные характеристики:")
         orbital.grid(row=5, column=3)
@@ -100,6 +104,27 @@ class Planet:
         E = Label(frame, font = ('Times New Roman',16,'bold'), bg = 'black', fg='white', text = ('Эксцентриситет: '+str(self.E)))
         E.grid(row=8, column=3)
         
+        empty_label3 = Label(frame, text= "____", bg="black", fg="black")
+        empty_label3.grid(row=9,column=3)
+        
+        atmo = Label(frame, font =('Times New Roman',20,'bold'), bg = 'black', fg='white', text=("Атмосфера:"+self.atmo))
+        atmo.grid(row=10, column=3)
+        
+        empty_label4 = Label(frame, text= "____", bg="black", fg="black")
+        empty_label4.grid(row=11,column=3)
+        
+        phis = Label(frame, font =('Times New Roman',20,'bold'), bg = 'black', fg='white', text="Физические характеристики:")
+        phis.grid(row=12,column=3)
+        
+        diam = Label(frame, font = ('Times New Roman',16,'bold'), bg = 'black', fg='white', text = ('Диаметр: '+str(self.diam)+' км.'))
+        diam.grid(row=13, column=3)
+        
+        grav = Label(frame, font = ('Times New Roman',16,'bold'), bg = 'black', fg='white', text = ('Гравитация: '+str(self.grav)+' м/c^2'))
+        grav.grid(row=14, column=3)
+        
+        mass = Label(frame, font = ('Times New Roman',16,'bold'), bg = 'black', fg='white', text = ('Масса: '+str(self.mass)+' кг'))
+        mass.grid(row=15, column=3)
+        
     def draw_orbit(self):
         if self.name != "Солнце":
             x, y = WIDTH/2-200, HEIGHT/2
@@ -108,15 +133,15 @@ class Planet:
                             width=1, outline="gray")
 
 # создаем сами планеты, задавая параметры (имя, размер, цвет, радиус орбиты, скорость вращения, начальный угол) в таком порядке
-sun =     Planet("Солнце",  50, "yellow",0,    0,   0,  19000,0,     0)
-mercury = Planet("Меркурий",10, "gray",  39,   1,   0,  167,  0.24,  0.21)
-venus =   Planet("Венера",  20, "orange",72,   0.9, 45, 464,  0.62,  0.01)
-earth =   Planet("Земля",   25, "blue",  100,  0.3, 90, 15,   1.00,  0.02)
-mars =    Planet("Марс",    20, "red",   152,  0.2, 135,-65,  1.88,  0.09)
-jupiter = Planet("Юпитер",  40, "brown", 200,  0.3, 180,-110, 11.86, 0.05)
-saturn =  Planet("Сатурн",  37, "brown", 300,  0.5, 225,-140, 29.46, 0.06)
-uranus =  Planet("Уран",    24, "blue",  350, 0.2, 270,-195,  84.01, 0.05)
-neptune = Planet("Нептун",  23, "blue",  400 ,0.13, 315,-200, 164.79,0.01)
+sun =     Planet("Солнце",  50, "yellow",0,    0,   0,  19000,0,     0   ,"-"                                                                       ,1391000,0   ,"1.99x10^30" )
+mercury = Planet("Меркурий",10, "gray",  39,   1,   0,  167,  0.24,  0.21,"Крайне разрежена,\n состоит из газов и пыли"                             ,4880   ,3.7 ,"3.3x10^23")
+venus =   Planet("Венера",  20, "orange",72,   0.9, 45, 464,  0.62,  0.01,"Плотная, состоит\n в основном из углекислого\n газа"                     ,12104  ,8.9 ,"4.87x10^24")
+earth =   Planet("Земля",   21, "blue",  100,  0.3, 90, 15,   1.00,  0.02,"Богата кислородом\n и азотом, с небольшим\n количеством углекислого газа",12742  ,9.8 ,"5.97x10^24")
+mars =    Planet("Марс",    10, "red",   152,  0.2, 135,-65,  1.88,  0.09,"Разрежена, состоит\n в основном из углекислого\nгаза и азота"            ,6779   ,3.7 ,"6.42x10^23")
+jupiter = Planet("Юпитер",  70, "brown", 200,  0.3, 180,-110, 11.86, 0.05,"Газовый гигант,\n состоит в основном из\nводорода и гелия"               ,139822 ,24.8,"1.90x10^27")
+saturn =  Planet("Сатурн",  50, "brown", 300,  0.5, 225,-140, 29.46, 0.06,"Газовый гигант,\nсостоит в основном из\nводорода и гелия"                ,116460 ,10.4,"5.68x10^26")
+uranus =  Planet("Уран",    40, "blue",  350, 0.2, 270,-195,  84.01, 0.05,"Газовый гигант,\nсостоит в основном из\nводорода и гелия"                ,50724  ,8.9 ,"8.68x10^25")
+neptune = Planet("Нептун",  35, "blue",  400 ,0.13, 315,-200, 164.79,0.01,"Газовый гигант,\nсостоит в основном из\nводорода и гелия"                ,49244  ,11.2,"1.02x10^26")
 
 # записываем планеты в массив
 planets = [sun, mercury, venus, earth, mars, jupiter, saturn, uranus, neptune]
